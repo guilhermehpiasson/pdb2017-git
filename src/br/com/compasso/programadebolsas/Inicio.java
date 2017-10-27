8b2c0e6af13bfb2397cf407a0e41474a11200d19
Guilherme Henrique Piasson <guilhermehpiasson@gmail.com>
2017-10-27 09:29:51 -0200

    Commit de fontes com um novo projeto

---
+++ src/br/com/compasso/programadebolsas/Inicio.java
@@ -0,0 +1,48 @@
+package br.com.compasso.programadebolsas;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="Inicio", urlPatterns = {"/inicio","/index"})
+public class Inicio extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public Inicio() {
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+	}
+
+	public void destroy() {
+	}
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Primeira Servlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Oi mundo Servlet!</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
